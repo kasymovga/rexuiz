9118c65a4e9e865c0d36a48e88fab32e797dd7b9
Grigorii Kasymov <kasymovga@gmail.com>
2023-02-10 10:32:45 +0500

    update

--- rexuiz-android/app/src/main/java/com/rexuiz/app/RexuizActivity.java
+++ rexuiz-android/app/src/main/java/com/rexuiz/app/RexuizActivity.java
@@ -15,6 +15,8 @@ public class RexuizActivity extends org.libsdl.app.SDLActivity {
         args[2] = "-userdir";
         args[3] = getExternalFilesDir(null) + "/";
         args[4] = "-rexuiz";
+        args[5] = "+gl_max_size";
+        args[6] = "256";
         return args;
     }
 }
